00c05e7b193d9d8d2a13f7dd15fd5b9e3f795e6e
shahad <shahad@gmail.com>
2024-07-29 15:00:46 +0530

    shift

--- hello-service-resilience4j-demo/src/main/java/com/handson/microservices/controller/Controller.java
+++ hello-service-resilience4j-demo/src/main/java/com/handson/microservices/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.microservices.demo.controller;
+package com.handson.microservices.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 //real world routes aren't gonna be like this tho
-@RequestMapping("/service1")
+@RequestMapping("/service")
 public class Controller {
-	
 	@Autowired
-	Environment environment;
+	Environment env;
+
+	
 	@GetMapping("/hello")
 	public ResponseEntity<?> sayHello(){
 		return ResponseEntity.status(200).body("hello service says Hello");
@@ -27,6 +29,6 @@ public class Controller {
 	
 	@GetMapping("/port")
 	public String getPort() {
-		return environment.getProperty("local.server.port");
+		return env.getProperty("server.port");
 	}
 }
