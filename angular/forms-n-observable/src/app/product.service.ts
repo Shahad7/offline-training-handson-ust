bd2aa356512972494eb59bbdadcb49c45b63307f
shahad <shahad@gmail.com>
2024-08-19 18:13:33 +0530

    shift:

--- angular/forms-n-observable/src/app/product.service.ts
+++ angular/forms-n-observable/src/app/product.service.ts
@@ -10,11 +10,11 @@ export class ProductService {
 
   constructor(private httpClient:HttpClient) { }
 
-   async saveProduct(product : Product){
+   saveProduct(product : Product){
     console.log(product)
     let url = "http://localhost:8082/api/v1/product"
-    //return this.httpClient.post(url,product)
-   await fetch(url,{method:"post",body:JSON.stringify(product),headers:{'Content-Type':'application/json'}})
+    this.httpClient.post(url,product).subscribe()
+   //fetch(url,{method:"post",body:JSON.stringify(product),headers:{'Content-Type':'application/json'}})
   }
 
 
